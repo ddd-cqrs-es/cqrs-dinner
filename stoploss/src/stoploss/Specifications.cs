4d12ff4fcf4916adaf80b7b4f1cb9145960c4335
Simon Laroche <laroche.simon@gmail.com>
2013-03-24 18:51:31 -0400

    Implemented simple stop loss
    Super simple stoploss, no trailing

--- stoploss/src/stoploss/Specifications.cs
+++ stoploss/src/stoploss/Specifications.cs
@@ -1,7 +1,77 @@
 ﻿namespace stoplosskata
 {
-    public class Specifications
-    {
-       
-    }
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using NUnit.Framework;
+
+	public abstract class Specification
+	{
+		protected abstract IEnumerable<Message> Given();
+		private List<Message> then;
+
+		[TestFixtureSetUp]
+		public void TestfixtureSetUp()
+		{
+			then = new List<Message>();
+
+			var sut = new Stoploss();
+
+			foreach (var message in Given())
+			{
+				var consume = typeof (IConsume<>).MakeGenericType(message.GetType()).GetMethod("Consume");
+				consume.Invoke(sut, new object[] {message});
+			}
+			
+			then.AddRange(sut.ProducedMessages);
+
+		}
+
+		protected IEnumerable<Message> Then
+		{
+			get { return then; }
+		}
+	}
+
+	[TestFixture]
+	public class When_a_position_is_taken: Specification
+	{
+		private Guid positionId;
+
+		protected override IEnumerable<Message> Given()
+		{
+			positionId = Guid.NewGuid();
+			yield return new PositionAcquiredMessage{Price = 10.0, PositionId = positionId, StopLoss = 1.0};
+			yield return new PriceChangedMessage{Price= 9.0}; 
+
+		}
+
+		[Test]
+		public void Release_position_sent()
+		{
+			Then.ContainsMessage(new SellPositionMessage {PositionId = positionId});
+		}
+
+		[Test]
+		public void Only_one_mmessage_produced()
+		{
+			Then.CountIs(1);
+		}
+
+	}
+
+	public static class TestExtension
+	{
+		public static void ContainsMessage<TMessage>(this IEnumerable<Message> messages, TMessage exprected) where TMessage : Message
+		{
+			CollectionAssert.Contains(messages, exprected);
+		}
+
+		public static void CountIs(this IEnumerable<Message> messages,int exprected)
+		{
+			Assert.That(messages.Count(), Is.EqualTo(exprected));
+			
+		}
+	}
+
 }
---
+++ stoploss/src/stoploss/Stoploss.cs
@@ -0,0 +1,115 @@
+﻿namespace stoplosskata
+{
+    using System;
+    using System.Collections.Generic;
+
+	public class Stoploss: IConsume<PositionAcquiredMessage>, IConsume<PriceChangedMessage>
+    {
+	    private Guid positionId;
+	    private double stopAt;
+	    private readonly IList<Message> producedMessages= new List<Message>();
+
+	    public void Consume(PositionAcquiredMessage message)
+        {
+	        positionId = message.PositionId;
+	        stopAt = message.Price - message.StopLoss;
+        }
+        public void Consume(PriceChangedMessage message)
+        {
+	        if (message.Price <= stopAt)
+	        {
+				producedMessages.Add(new SellPositionMessage{PositionId = positionId});    
+	        }
+        }
+        
+
+        public IEnumerable<Message> ProducedMessages
+        {
+	        get {
+		        foreach (var message in producedMessages)
+		        {
+			        yield return message;
+		        }
+		    }
+	       
+        }
+    }
+
+    public class SellPositionMessage: Message, IEquatable<SellPositionMessage>
+    {
+	    public bool Equals(SellPositionMessage other)
+	    {
+		    if (ReferenceEquals(null, other))
+		    {
+			    return false;
+		    }
+		    if (ReferenceEquals(this, other))
+		    {
+			    return true;
+		    }
+		    return PositionId.Equals(other.PositionId);
+	    }
+
+	    public override bool Equals(object obj)
+	    {
+		    if (ReferenceEquals(null, obj))
+		    {
+			    return false;
+		    }
+		    if (ReferenceEquals(this, obj))
+		    {
+			    return true;
+		    }
+		    if (obj.GetType() != this.GetType())
+		    {
+			    return false;
+		    }
+		    return Equals((SellPositionMessage) obj);
+	    }
+
+	    public override int GetHashCode()
+	    {
+		    return PositionId.GetHashCode();
+	    }
+
+	    public static bool operator ==(SellPositionMessage left, SellPositionMessage right)
+	    {
+		    return Equals(left, right);
+	    }
+
+	    public static bool operator !=(SellPositionMessage left, SellPositionMessage right)
+	    {
+		    return !Equals(left, right);
+	    }
+
+	    public Guid PositionId { get; set; }
+    }
+
+    public class Message
+    {
+	    public Message()
+	    {
+			Id = Guid.NewGuid();
+	    }
+
+	    public Guid Id ;
+    }
+
+    public class PositionAcquiredMessage: Message
+    {
+	    public Guid PositionId { get; set; }
+	    public double Price { get; set; }
+
+	    public double StopLoss { get; set; }
+    }
+
+    public class PriceChangedMessage: Message
+    {
+	    public double Price { get; set; }
+    }
+
+	public interface IConsume<T> where T: Message
+	{
+		void Consume(T message);
+	}
+}
