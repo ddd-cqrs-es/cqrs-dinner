be0f9ef3745b27f358f08c2f8202b9fc2dea7f6a
Simon Laroche <laroche.simon@gmail.com>
2013-05-13 14:50:21 -0400

    added program file

---
+++ stoploss/src/stoploss/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace stoplosskata
+{
+    using System;
+    using NUnit.Framework;
+
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            A();
+            return 0;
+        }
+
+        private static void A()
+        {
+            Console.WriteLine("A");
+            B();
+            C();
+        }
+
+        private static void C()
+        {
+            Console.WriteLine("C");
+            D();
+        }
+
+        private static void D()
+        {
+            Console.WriteLine("D");
+        }
+
+        private static void B()
+        {
+            Console.WriteLine("B");
+        }
+
+    }
+
+    [TestFixture]
+    public class ProgramTest
+    {
+        [Test]
+        public void Test_program()
+        {
+            Assert.That( Program.Main(new string[0]), Is.EqualTo(0));
+        }
+    }
+}
