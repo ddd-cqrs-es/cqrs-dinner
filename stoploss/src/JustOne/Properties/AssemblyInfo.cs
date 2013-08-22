de1236dc093ec98ecb7b20eccb7374f21a5bcfec
Simon Laroche <laroche.simon@gmail.com>
2013-08-22 19:56:27 -0400

    Mutex sample, removed unnecessary projects

--- stoploss/src/JustOne/Properties/AssemblyInfo.cs
+++ stoploss/src/JustOne/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("stoploss.tests")]
+[assembly: AssemblyTitle("JustOne")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("stoploss.tests")]
+[assembly: AssemblyProduct("JustOne")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0cf4d9f6-cdfe-464a-913b-8f967da56f42")]
+[assembly: Guid("61e93043-f52a-4559-b51f-a6525cf3ed74")]
 
 // Version information for an assembly consists of the following four values:
 //
--- stoploss/src/tutorials/ThreadTests/SynchronizationTests.cs
+++ stoploss/src/tutorials/ThreadTests/SynchronizationTests.cs
@@ -2,9 +2,13 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
     using System.Threading;
     using System.Threading.Tasks;
     using NUnit.Framework;
+    using ThreadState = System.Threading.ThreadState;
 
     public class ThreadUnsafe
     {
@@ -48,6 +52,41 @@
                     Console.WriteLine(val1/val2);
                     val2 = 0;
                 }
+            }
+        }
+    }
+
+    public class ThreadSafeMonitor
+    {
+        private static int val1 = 1;
+        private static int val2 = 1;
+        private static bool firstPass = true;
+        private static readonly object locker = new object();
+
+        public static void Go()
+        {
+            bool lockTaken = false;
+            try
+            {
+
+                Monitor.Enter(locker, ref lockTaken);
+
+                if (val2 != 0)
+                {
+                    if (firstPass)
+                    {
+                        firstPass = false;
+                        Thread.Sleep(10);
+                    }
+
+
+                    Console.WriteLine(val1/val2);
+                    val2 = 0;
+                }
+            }
+            finally
+            {
+                if(lockTaken){Monitor.Enter(locker);}
             }
         }
     }
@@ -106,5 +145,112 @@
                 Assert.Fail("No exception should be thrown");
             }
         }
+
+        [Test]
+        public void No_throws_divide_by_zero_monitor()
+        {
+            var tasks = new List<Task>();
+            for (int i = 0; i < 2; i++)
+            {
+
+                var task = Task.Factory.StartNew(ThreadSafeMonitor.Go);
+                tasks.Add(task);
+            }
+
+            try
+            {
+                foreach (var task in tasks)
+                {
+                    task.Wait();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("No exception should be thrown");
+            }
+        }
+
+        [Test]
+        public void DeadLockTest()
+        {
+            var thread = new Thread(DeadLock.Go);
+            var thread1 = new Thread(DeadLock.Go2);
+            thread.Start();
+            thread1.Start();
+
+            Thread.Sleep(10000);
+
+            //we seem to be locked
+            Assert.That(thread.ThreadState, Is.EqualTo(ThreadState.WaitSleepJoin ));
+            Assert.That(thread1.ThreadState, Is.EqualTo(ThreadState.WaitSleepJoin));
+            
+            thread.Abort();
+            thread1.Abort();
+
+
+        }
+
+        [Test]
+        public void Mutex_in_other_process()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var p = new Process {StartInfo = {WindowStyle = ProcessWindowStyle.Hidden, CreateNoWindow = true, FileName = "JustOne.exe"}};
+
+                p.Start();
+            }
+
+            Thread.Sleep(10000); // wait a bit for all the process to close
+
+            var processes = Process.GetProcessesByName("JustOne");
+            try
+            {
+                Assert.That(processes.Count(), Is.EqualTo(1));
+            }
+            finally
+            {
+                try
+                {
+                    foreach (var process in processes)
+                    {
+                        process.Kill();
+                    }
+                }catch{}
+
+            }
+        }
     }
+
+    public class DeadLock
+    {
+        static object locker1 = new object();
+        static object locker2 = new object();
+
+        public static void Go()
+        {
+            lock (locker1)
+            {
+                Thread.Sleep(1000);
+                lock (locker2)
+                {
+                    
+                }
+            }
+        }
+
+        public static void Go2()
+        {
+            lock (locker2)
+            {
+                Thread.Sleep(1000);
+                lock (locker1)
+                {
+                    
+                }
+            }
+        }
+    }
+
+
 }
