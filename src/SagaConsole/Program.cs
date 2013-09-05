5d9d299f692acd2fed8e2019fceec70caf6aba93
Simon Laroche <laroche.simon@gmail.com>
2013-09-05 14:32:25 -0400

    Saga with routing slip from http://vasters.com/clemensv/2012/09/01/Sagas.aspx

--- src/SagaConsole/Program.cs
+++ src/SagaConsole/Program.cs
@@ -8,8 +8,43 @@ namespace SagaConsole
 {
 	class Program
 	{
+		static ActivityHost[] processes;
+
 		static void Main(string[] args)
 		{
+			var routingSlip = new RoutingSlip(new WorkItem[]
+                {
+                    new WorkItem<ReserveCarActivity>(new WorkItemArguments{{"vehicleType", "Compact"}}),
+                    new WorkItem<ReserveHotelActivity>(new WorkItemArguments{{"roomType", "Suite"}}),
+                    new WorkItem<ReserveFlightActivity>(new WorkItemArguments{{"destination", "DUS"}})
+                });
+
+
+			// imagine these being completely separate processes with queues between them
+			processes = new ActivityHost[]
+                                {
+                                    new ActivityHost<ReserveCarActivity>(Send),
+                                    new ActivityHost<ReserveHotelActivity>(Send),
+                                    new ActivityHost<ReserveFlightActivity>(Send)
+                                };
+
+			// hand off to the first address
+			Send(routingSlip.ProgressUri, routingSlip);
+			Console.WriteLine("Press any key the stop this program...");
+			Console.ReadLine();
 		}
+
+		static void Send(Uri uri, RoutingSlip routingSlip)
+		{
+			// this is effectively the network dispatch
+			foreach (var process in processes)
+			{
+				if (process.AcceptMessage(uri, routingSlip))
+				{
+					break;
+				}
+			}
+		}
+
 	}
 }
