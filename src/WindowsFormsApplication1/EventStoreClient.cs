962a67b69251bf3a85d6da43bdcda4800e2a811e
Simon Laroche <laroche.simon@gmail.com>
2013-09-20 16:44:11 -0400

    chat client

---
+++ src/WindowsFormsApplication1/EventStoreClient.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApplication1
+{
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Linq;
+	using System.Net;
+	using System.ServiceModel.Syndication;
+	using System.Xml;
+
+	public class EventStoreClient
+	{
+		public static SyndicationLink GetNamedLink(IEnumerable<SyndicationLink> links, string name)
+		{
+			return links.FirstOrDefault(link => link.RelationshipType == name);
+		}
+
+		public static Uri GetLast(Uri head)
+		{
+			var request = (HttpWebRequest) WebRequest.Create(head);
+			request.Credentials = new NetworkCredential("admin", "changeit");
+			request.Accept = "application/atom+xml";
+			try
+			{
+				using (var response = (HttpWebResponse) request.GetResponse())
+				{
+					if (response.StatusCode == HttpStatusCode.NotFound)
+					{
+						return null;
+					}
+					using (var xmlreader = XmlReader.Create(response.GetResponseStream()))
+					{
+						var feed = SyndicationFeed.Load(xmlreader);
+						var last = GetNamedLink(feed.Links, "last");
+						return (last != null) ? last.Uri : GetNamedLink(feed.Links, "self").Uri;
+					}
+				}
+			}
+			catch (WebException ex)
+			{
+				if (((HttpWebResponse) ex.Response).StatusCode == HttpStatusCode.NotFound)
+				{
+					return null;
+				}
+				throw;
+			}
+		}
+
+		public static void ProcessItem(SyndicationItem item, Action<string> read)
+		{
+			Console.WriteLine(item.Title.Text);
+			//get events
+			var request = (HttpWebRequest) WebRequest.Create(GetNamedLink(item.Links, "alternate").Uri);
+			request.Credentials = new NetworkCredential("admin", "changeit");
+			request.Accept = "application/json";
+			using (var response = request.GetResponse())
+			{
+				var streamReader = new StreamReader(response.GetResponseStream());
+
+				var messaqge = streamReader.ReadToEnd();
+				read(messaqge);
+			}
+		}
+
+		public static Uri ReadPrevious(Uri uri, Action<string> read)
+		{
+			var request = (HttpWebRequest) WebRequest.Create(uri);
+			request.Credentials = new NetworkCredential("admin", "changeit");
+			request.Accept = "application/atom+xml";
+			using (var response = request.GetResponse())
+			{
+				using (var xmlreader = XmlReader.Create(response.GetResponseStream()))
+				{
+					var feed = SyndicationFeed.Load(xmlreader);
+					foreach (var item in feed.Items.Reverse())
+					{
+						ProcessItem(item, read);
+					}
+					var prev = GetNamedLink(feed.Links, "previous");
+					return prev == null ? uri : prev.Uri;
+				}
+			}
+		}
+
+		public static void PostMessage(string user, string text)
+		{
+			var message = @"[{'eventType':'chatMessage', 'eventId' :
+				'" + Guid.NewGuid() + @"',
+				'data' :
+				{
+					'user': '" + user + @"',
+					'text':  '" + text + @"' 
+					}}]";
+			var request = WebRequest.Create("http://ha.geteventstore.com:2113/streams/chat-foo");
+			request.Method = "POST";
+			request.ContentType = "application/json";
+			request.ContentLength = message.Length;
+			using (var sw = new StreamWriter(request.GetRequestStream()))
+			{
+				sw.Write(message);
+			}
+			using (var response = request.GetResponse())
+			{
+				response.Close();
+			}
+		}
+	}
+}
---
+++ src/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+	using System.Collections.Concurrent;
+	using System.Threading;
+	
+	public partial class Form1 : Form
+	{
+		private ConcurrentQueue<Tuple<string, string>> queue = new ConcurrentQueue<Tuple<string, string>>(); 
+		public Form1()
+		{
+			InitializeComponent();
+
+			var t = new Thread(Poll);
+			t.Start();
+
+			var q = new Thread(Post);
+			q.Start();
+
+		}
+
+		private void Post()
+		{
+			while (true)
+			{
+				Tuple<string, string> item;
+				queue.TryPeek(out item);
+
+				if (item != null)
+				{
+					EventStoreClient.PostMessage("Big bucket", textBox1.Text);
+					Tuple<string, string> dequeud;
+					queue.TryDequeue(out dequeud);
+				}
+
+			}
+		}
+
+		private void Poll()
+		{
+
+
+			Uri last = null;
+			
+			while (last == null)
+			{
+				last = EventStoreClient.GetLast(new Uri("http://ha.geteventstore.com:2113/streams/chat-foo"));
+				if (last == null)
+				{
+					Thread.Sleep(1000);
+				}
+			}
+
+			while (true)
+			{
+				var current = EventStoreClient.ReadPrevious(last, x => this.BeginInvoke(new Action(() => AddLine(x))));
+
+				if (last == current)
+				{
+					Thread.Sleep(1000);
+				}
+				 last = current;
+			}
+
+		}
+
+		void AddLine(string text)
+		{
+			richTextBox1.AppendText( text);
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			queue.Enqueue(new Tuple<string, string>("Big bucket", textBox1.Text));
+		}
+	}
+}
