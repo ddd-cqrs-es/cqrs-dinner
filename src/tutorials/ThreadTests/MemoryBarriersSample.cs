99d0f38500201dfbb8c2aeb2bb023f04c841c349
Simon Laroche <laroche.simon@gmail.com>
2013-10-24 22:31:41 -0400

    Wait pulse queue

--- src/tutorials/ThreadTests/MemoryBarriersSample.cs
+++ src/tutorials/ThreadTests/MemoryBarriersSample.cs
@@ -1,7 +1,61 @@
 ﻿namespace tutorials.ThreadTests
 {
+	using System;
+	using System.Threading;
+
 	public class MemoryBarriersSample
 	{
-		 
+		 public class Foo
+		 {
+			 private int answer;
+			 private bool complete;
+
+			 public void A()
+			 {
+				 answer = 123;
+				 Thread.MemoryBarrier();
+				 complete = true;
+				 Thread.MemoryBarrier();
+
+
+			 }
+
+			 public void B()
+			 {
+				 Thread.MemoryBarrier();
+				 if (complete)
+				 {
+					 Thread.MemoryBarrier();
+					 Console.WriteLine(answer);
+				 }
+			 }
+
+		 }
+	}
+
+	public class VolatileSample
+	{
+		public class Foo
+		{
+			private int answer;
+			private volatile bool complete;
+
+			public void A()
+			{
+				answer = 123;
+				complete = true;
+				
+
+			}
+
+			public void B()
+			{
+				if (complete)
+				{
+					Console.WriteLine(answer);
+				}
+			}
+
+		}
 	}
 }
---
+++ src/tutorials/ThreadTests/WaitPulseProducerConsumerQueue.cs
@@ -0,0 +1,97 @@
+﻿namespace tutorials.ThreadTests
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Threading;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class WaitPulseQueueSample
+	{
+		[Test]
+		public void Main()
+		{
+			var queue = new WaitPulseProducerConsumerQueue(3);
+			var random = new Random(DateTime.Now.Millisecond);
+			Console.WriteLine("Enqueuing 10 items");
+			for (int i = 0; i < 100; i++)
+			{
+				int itemNumber = i;
+				queue.EnqueueItem(()=>{
+					                      int millisecondsTimeout = (itemNumber%5)*1000;
+					                      Thread.Sleep(millisecondsTimeout);
+					Console.WriteLine(" Task " + itemNumber + " slept " + millisecondsTimeout + " ms");
+				});
+			}
+
+			queue.Shutdown(true);
+
+			Console.WriteLine("Worker complete!");
+		}
+	}
+
+	public class WaitPulseProducerConsumerQueue
+	{
+		private readonly Thread[] workers;
+		private Queue<Action> queue= new Queue<Action>();
+ 		private readonly object locker = new object();
+
+		public WaitPulseProducerConsumerQueue(int workerCount)
+		{
+			workers= new Thread[workerCount];
+
+			for (int i = 0; i < workerCount; i++)
+			{
+				var thread = new Thread(Consume);
+				thread.Start(i);
+				workers[i]=thread;
+			}
+		}
+
+		private void Consume(object data)
+		{
+			while (true)
+			{
+				Action action;
+				lock (locker)
+				{
+					while (queue.Count == 0) Monitor.Wait(locker); //this makes cunsumer pull  
+
+
+					action = queue.Dequeue();
+				}
+				if(action == null) return;
+					Console.Write("Consumer " + data + " ");
+					action(); //invoke action
+				
+			}
+		}
+
+		
+
+		public void Shutdown(bool waitForWorker)
+		{
+			foreach (var worker in workers)
+			{
+				EnqueueItem(null);
+			}
+
+			if (waitForWorker)
+			{
+				foreach (var worker in workers)
+				{
+					worker.Join();
+				}
+			}
+		}
+
+		public void EnqueueItem(Action action)
+		{
+			lock (locker)
+			{
+				queue.Enqueue(action);
+				Monitor.Pulse(locker);
+			}
+		}
+	}
+}
