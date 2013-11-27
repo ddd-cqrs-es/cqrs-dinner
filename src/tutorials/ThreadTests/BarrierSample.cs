f780e4388c2398da85aa59a94e2663c080803dc5
Simon Laroche <laroche.simon@gmail.com>
2013-11-26 21:13:50 -0500

    Barrier sampl

---
+++ src/tutorials/ThreadTests/BarrierSample.cs
@@ -0,0 +1,32 @@
+﻿namespace tutorials.ThreadTests
+{
+	using System;
+	using System.Threading;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class BarrierSample
+	{
+		private Barrier barrier;
+
+		[Test]
+		public void Main()
+		{
+			barrier = new Barrier(3);
+			new Thread(Speak).Start("A");
+			new Thread(Speak).Start("B");
+			new Thread(Speak).Start("C");
+
+
+		}
+
+		private void Speak(object id)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				Console.WriteLine(id.ToString() + i + " ");
+				barrier.SignalAndWait();
+			}
+		}
+	}
+}
