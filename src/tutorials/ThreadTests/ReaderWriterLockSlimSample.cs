d73cbdf095d8e3410fa5f6145f3d0a3dec6c84fb
Simon Laroche <laroche.simon@gmail.com>
2013-11-28 21:26:53 -0500

    Upgrade lock

--- src/tutorials/ThreadTests/ReaderWriterLockSlimSample.cs
+++ src/tutorials/ThreadTests/ReaderWriterLockSlimSample.cs
@@ -21,12 +21,15 @@
 			new Thread(Write).Start("A");
 			new Thread(Write).Start("B");
 
+			Thread.Sleep(10000);
+
 		}
 
 		private void Write(object tid)
 		{
 			while (true)
 			{
+				Console.WriteLine(rw.CurrentReadCount + " concurrent readers");
 				int newNumber = GetRandNum(100);
 				rw.EnterWriteLock();
 				items.Add(newNumber);
@@ -57,4 +60,66 @@
 		}
 
 	}
+
+	[TestFixture]
+	public class ReaderWriterLockSlim_upgrade_sample
+	{
+		ReaderWriterLockSlim rw = new ReaderWriterLockSlim();
+		List<int> items = new List<int>();
+		Random rnd = new Random();
+
+		[Test]
+		public void Main()
+		{
+			new Thread(Read).Start();
+			new Thread(Read).Start();
+			new Thread(Read).Start();
+			new Thread(Write).Start("A");
+			new Thread(Write).Start("B");
+
+			Thread.Sleep(10000);
+
+		}
+
+		private void Write(object tid)
+		{
+			while (true)
+			{
+				Console.WriteLine(rw.CurrentReadCount + " concurrent readers");
+				int newNumber = GetRandNum(100);
+				rw.EnterUpgradeableReadLock();
+				if (!items.Contains(newNumber))
+				{
+					rw.EnterWriteLock();
+					items.Add(newNumber);
+					rw.ExitWriteLock();
+					Console.WriteLine("Thread " + tid + " added " + newNumber);
+				}
+				rw.ExitUpgradeableReadLock();
+			}
+		}
+
+		private int GetRandNum(int max)
+		{
+			lock (rnd)
+			{
+				return rnd.Next(max);
+			}
+		}
+
+		private void Read()
+		{
+			while (true)
+			{
+				rw.EnterReadLock();
+				foreach (var item in items)
+				{
+					Thread.Sleep(10);
+				}
+				rw.ExitReadLock();
+			}
+		}
+
+	}
+
 }
