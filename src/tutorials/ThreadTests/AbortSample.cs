e78bcb04517006294c845f7498e3d10e9c72a12e
Simon Laroche <laroche.simon@gmail.com>
2013-12-02 21:05:29 -0500

    Abort sample

---
+++ src/tutorials/ThreadTests/AbortSample.cs
@@ -0,0 +1,28 @@
+﻿namespace tutorials.ThreadTests
+{
+	using System;
+	using System.Threading;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class AbortSample
+	{
+		[Test]
+		public void Main()
+		{
+			var t = new Thread(() =>
+				{ while (true) ; });
+			Console.WriteLine(t.ThreadState); //unstarted
+			t.Start();
+			Thread.Sleep(1000);
+			Console.WriteLine(t.ThreadState); // runnning
+
+			t.Abort();
+			Console.WriteLine(t.ThreadState); //Abort requested
+
+			t.Join();
+			Console.WriteLine(t.ThreadState); //Aborted
+		}
+ 
+	}
+}
