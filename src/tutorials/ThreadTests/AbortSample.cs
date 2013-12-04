4397adc6949ffb4c1836d6b9e8ccf604e0bebfc5
Simon Laroche <laroche.simon@gmail.com>
2013-12-03 20:52:54 -0500

    Thread abort complications

--- src/tutorials/ThreadTests/AbortSample.cs
+++ src/tutorials/ThreadTests/AbortSample.cs
@@ -1,12 +1,14 @@
 ﻿namespace tutorials.ThreadTests
 {
 	using System;
+	using System.IO;
 	using System.Threading;
 	using NUnit.Framework;
 
 	[TestFixture]
 	public class AbortSample
 	{
+
 		[Test]
 		public void Main()
 		{
@@ -87,6 +89,63 @@
 					Console.WriteLine("I wwill not die!");
 				}
 			}
+		}
+
+		[Test]
+		public void Thread_abort_complication()
+		{
+			turnstyle = new AutoResetEvent(false);
+			var t = new Thread(Complicated);
+
+			t.Start();
+
+			t.Abort();
+
+			t.Join();
+
+			Assert.That(IsOpen(), Is.True);
+
+			
+		}
+
+		[TearDown]
+		public void CloseFile()
+		{
+			if (sw != null)
+			{
+				sw.Dispose();
+			}
+		}
+
+		private bool IsOpen()
+		{
+			try
+			{
+				File.Open("myfile.txt", FileMode.Open);
+			}
+			catch (IOException e)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		private AutoResetEvent turnstyle;
+		private StreamWriter sw;
+
+		private void Complicated()
+		{
+			//Equivalent to:
+			sw = File.CreateText("myfile.txt");
+			turnstyle.WaitOne();
+			// if we get a signal tp abort the  file never gets closed
+			try{ sw.Write("About-safe?");}
+			finally{ sw.Dispose(); }
+			
+//			using (var sw = File.CreateText("myfile.txt"))
+//			{
+//				sw.Write("About-safe?");
+//			}
 		}
 	}
 }
